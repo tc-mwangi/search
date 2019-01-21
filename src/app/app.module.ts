12b48de6fda61b78953fa67314d704a39421ed42
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-01-22 01:04:11 +0300

    generate new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    ProfileComponent,
+    SearchComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
