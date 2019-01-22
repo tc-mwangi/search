52c8e34a4fd08850dbefe69c6d32aa3537b85aec
Loise Mwangi <wanjirumwangi.learn@gmail.com>
2019-01-22 06:09:56 +0300

    edit search and profile footers

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,6 +1,7 @@
 export class User {
-
+  constructor(public name:string, public login:string, public location:string, public avatar_url:string, public bio:string,public email:string, public followers:number, public following:number, public public_repos:number, public html_url:string){}
 }
+
 
 // {
 //   "login": "tc-mwangi",
